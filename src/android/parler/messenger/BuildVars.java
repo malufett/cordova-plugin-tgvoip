2b7f46226846f4b1b4e263e60c437f70bbd7a325
Rudolp <malufett.eat.my.binaries@gmail.com>
2021-06-14 15:21:38 -0400

    Follow up
    Signed-off-by: Rudolp <malufett.eat.my.binaries@gmail.com>

--- src/android/parler/messenger/BuildVars.java
+++ src/android/parler/messenger/BuildVars.java
@@ -15,7 +15,7 @@ public class BuildVars {
 
     public static boolean DEBUG_VERSION = false;
     public static boolean DEBUG_PRIVATE_VERSION = true;
-    public static boolean LOGS_ENABLED = true;
+    public static boolean LOGS_ENABLED = false;
     public static boolean USE_CLOUD_STRINGS = true;
     public static boolean CHECK_UPDATES = false;
     public static int BUILD_VERSION = 2206;
