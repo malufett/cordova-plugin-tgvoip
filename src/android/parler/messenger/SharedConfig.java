b19f7488bba6417d98413fa8262e86a8a3de744e
Rudolp <malufett.eat.my.binaries@gmail.com>
2021-06-17 13:46:49 -0400

    Follow up
    Signed-off-by: Rudolp <malufett.eat.my.binaries@gmail.com>

--- src/android/parler/messenger/SharedConfig.java
+++ src/android/parler/messenger/SharedConfig.java
@@ -29,7 +29,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
-import androidx.core.content.pm.ShortcutManagerCompat;
+// import androidx.core.content.pm.ShortcutManagerCompat;
 
 public class SharedConfig {
 
