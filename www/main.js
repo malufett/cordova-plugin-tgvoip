73cd86f9ab20c053d4bbdc61b36fe8d36724afd8
Rudolp <malufett.eat.my.binaries@gmail.com>
2021-06-17 08:25:47 -0400

    Follow up
    Signed-off-by: Rudolp <malufett.eat.my.binaries@gmail.com>

--- www/main.js
+++ www/main.js
@@ -5,19 +5,19 @@ module.exports = {
         cordova.exec(successCallback, errorCallback, "TGVoipPlugin", "createCall", [params, GA, isOutgoing]);
     },
     generateGA: function (random, successCallback, errorCallback) {
-        successCallback = function (result) {
+        var successCallback_ = function (result) {
             result.g_a = new Uint8Array(result.g_a);
             result.g_a_hash = new Uint8Array(result.g_a_hash);
             result.a_or_b = new Uint8Array(result.a_or_b);
             successCallback(result);
         };
-        cordova.exec(successCallback, errorCallback, "TGVoipPlugin", "generateG_A", [random]);
+        cordova.exec(successCallback_, errorCallback, "TGVoipPlugin", "generateG_A", [random]);
     },
     generateGB: function (random, successCallback, errorCallback) {
-        successCallback = function (result) {
+        var successCallback_ = function (result) {
             successCallback(new Uint8Array(result));
         };
-        cordova.exec(successCallback, errorCallback, "TGVoipPlugin", "generateG_B", [random]);
+        cordova.exec(successCallback_, errorCallback, "TGVoipPlugin", "generateG_B", [random]);
     },
     generateFingerPrint: function (gb, aorb, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "TGVoipPlugin", "generateFingerprint", [gb, aorb]);
